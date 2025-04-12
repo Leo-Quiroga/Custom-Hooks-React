82d37b2e468889f84ff650daf1b4a67881bb955f
Hernán Leonardo Quiroga Beltrán <hleonardoquirogab@hotmail.com>
2025-04-12 17:40:36 -0500

    Repositorio para guardar 3 hooks personalizados sobre contadores, fetch y forms

---
+++ src/components/FetchApp.jsx
@@ -0,0 +1,58 @@
+import React, { useEffect } from 'react'
+import { useFetch } from '../hooks/useFetch'
+
+export const FetchApp = () => {
+
+    const url = 'https://jsonplaceholder.typicode.com/users'
+
+
+    const { data, isLoading, error, fetchData } = useFetch()
+
+    useEffect(() => {
+      fetchData(url, 'GET')
+    }, [url])
+    
+
+    return (
+        <>
+            <h2>Lista</h2>
+
+            {isLoading
+                ? <h4> Cargando...</h4>
+                : error
+                    ? <h4>Hubo un error: {error}</h4>
+                    : <table className="table">
+                        <thead>
+                            <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">Name</th>
+                                <th scope="col">Email</th>
+                                <th scope="col">Website</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                data.map(user => {
+                                    return(
+                                        <tr key={user.id}>
+                                        <th scope="row">{user.id}</th>
+                                        <td>{user.name}</td>
+                                        <td>{user.email}</td>
+                                        <td>{user.website}</td>
+                                    </tr>
+                                    )
+                                }
+                                   
+                                )
+                            }
+
+                        </tbody>
+                    </table>
+            }
+
+
+
+        </>
+
+    )
+}
